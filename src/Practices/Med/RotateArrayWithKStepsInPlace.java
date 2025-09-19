a7a83d8582730bfbcd23e100290856a63c395634
Janky Lewis <vinhtranak02092k@gmail.com>
2025-09-19 17:42:06 +0700

    Kept practicing

---
+++ src/Practices/Med/RotateArrayWithKStepsInPlace.java
@@ -0,0 +1,51 @@
+package Practices.Med;
+
+import java.util.Arrays;
+
+public class RotateArrayWithKStepsInPlace {
+    
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sol(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3)));
+    }
+
+    static int[] sol(int[] arr, int k) {
+
+        // Rotate the entire arr
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        // Rotate the first k elements
+        left = 0;
+        right = k - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        // Rotate the remaining after the first k elements
+        left = k;
+        right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            
+            left++;
+            right--;
+        }
+
+        return arr;
+    }
+}
